44fda3b285717d44d18c752e29ea4388828112e3
Diego Chagas <diegofchagas@gmail.com>
2024-02-16 20:45:25 -0300

    add nova pagina Home e add a mesma as rotas

--- src/Router.tsx
+++ src/Router.tsx
@@ -5,6 +5,7 @@ import { DefaultLayout } from "./Layout/DefaultLayout"
 import { Checkout } from "./pages/Checkout"
 import { CheckoutFiled } from "./pages/CheckoutFiled"
 import { Success } from "./pages/Success"
+import { Home } from "./pages/Home"
 
 
 
@@ -12,6 +13,7 @@ export const Router = () => {
   return (
     <Routes>
       <Route path="/" element={<DefaultLayout/>}>
+      <Route path="/" element={<Home/>}/>
         <Route path="/checkout" element={<Checkout/>}/>
         <Route path="/checkoutFiled" element={<CheckoutFiled/>}/>
         <Route path="/success" element={<Success/>}/>
