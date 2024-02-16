3c5113a4a2e9e6f77777001c0c231be6f576f359
Diego Chagas <diegofchagas@gmail.com>
2024-02-16 18:45:06 -0300

    adicionando pagina de sucesso de pedido e adicionando paginas criadas nas rotas

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,13 +1,20 @@
 import {Routes, Route} from "react-router-dom"
+
 import { DefaultLayout } from "./Layout/DefaultLayout"
+
+import { Checkout } from "./pages/Checkout"
+import { CheckoutFiled } from "./pages/CheckoutFiled"
+import { Success } from "./pages/Success"
 
 
 
 export const Router = () => {
   return (
     <Routes>
-      <Route>
-        <Route path="/" element={<DefaultLayout/>}/>
+      <Route path="/" element={<DefaultLayout/>}>
+        <Route path="/checkout" element={<Checkout/>}/>
+        <Route path="/checkoutFiled" element={<CheckoutFiled/>}/>
+        <Route path="/success" element={<Success/>}/>
       </Route>
     </Routes>
   )
