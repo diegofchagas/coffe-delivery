9c61b5a7ec4fc425221d9b50c252448cb10610da
Diego Chagas <diegofchagas@gmail.com>
2024-02-16 18:39:25 -0300

    adicionando theme em todo o projeto

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import { GlobalStyle } from './styles/global.ts'
+import { ThemeProvider } from 'styled-components'
+import { defaultTheme } from './styles/themes/defatult.ts'
 
 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    <ThemeProvider theme={defaultTheme}>
     <App />
+    <GlobalStyle/>
+    </ThemeProvider>
   </React.StrictMode>,
 )
