ef2725b08afe467346bd7db84d069ca61622065f
Diego Chagas <diegofchagas@gmail.com>
2024-02-16 18:49:58 -0300

    adicionando novas cores ao theme

--- src/styles/themes/defatult.ts
+++ src/styles/themes/defatult.ts
@@ -11,4 +11,12 @@ export const defaultTheme = {
   'base-button': '#E6E5E5',
   'base-input': '#EDEDED',
   'base-card': '#F3F2F2',
+
+  'yellow-light': '#F1E9C9',
+  'yellow': '#DBAC2C',
+  'yellow-dark': '#c47f17',
+
+  'purple-light': '#EBE5F9',
+  'purple': '#8047F8',
+  'purle-dark': '#4B2995',
 }
