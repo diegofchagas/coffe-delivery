05b1d3f3f078d435af731d81ec98ec481cce1bc1
Diego Chagas <diegofchagas@gmail.com>
2024-02-17 16:14:05 -0300

    ajustando estrutura e estilizacao do componente About

--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -1,24 +1,30 @@
-import copo from "../../assets/copo-cafe.svg"
-import carrinho from "../../assets/carrinho.svg"
-import embalagem from "../../assets/embalagem.svg"
-import relogio from "../../assets/relogio.svg"
-import cafe from "../../assets/cafe.svg"
+import copo from "../../assets/copo-cafe.svg";
+import carrinho from "../../assets/carrinho.svg";
+import embalagem from "../../assets/embalagem.svg";
+import relogio from "../../assets/relogio.svg";
+import cafe from "../../assets/cafe.svg";
+import { Banner, ContainerAbout, InfoContainer } from "./style";
 
 export const About = () => {
   return (
-    <div>
-      <h1>Encontre o café perfeito para qualquer hora do dia</h1>
-
-      <p>Com o Coffee Delivery você recebe seu café onde estiver, a qualquer hora</p>
-
+    <ContainerAbout>
+      <Banner>
+        <h1>
+          Encontre o café perfeito <br />
+          para qualquer hora do dia
+        </h1>
+        <p>
+          Com o Coffee Delivery você recebe seu café onde estiver, a <br />{" "}
+          qualquer hora
+        </p>
+      </Banner>
       <img src={copo} alt="" />
-
-      <div>
-       <img src={carrinho} alt="" /> <span>Compra simples e segura</span>
-       <img src={relogio} alt="" /> <span>Entrega rápida e rastreada</span>
-        <img src={embalagem} alt="" /><span>Embalagem mantém o café intacto</span>
-       <img src={cafe} alt="" /> <span>O café chega fresquinho até você</span>
-      </div>
-    </div>
-  )
-}
+      <InfoContainer>
+        <img src={carrinho} alt="" /> <span>Compra simples e segura</span>
+        <img src={embalagem} alt="" /> <span>Embalagem mantém o café intacto</span>
+        <img src={relogio} alt="" /> <span>Entrega rápida e rastreada</span>
+        <img src={cafe} alt="" /> <span>O café chega fresquinho até você</span>
+      </InfoContainer>
+    </ContainerAbout>
+  );
+};
---
+++ src/components/About/style.ts
@@ -0,0 +1,55 @@
+import styled from 'styled-components';
+
+export const ContainerAbout = styled.section`
+  margin-top:5rem;
+  display:grid;
+  grid-template-columns: 2fr 1fr;
+  
+  img{
+    grid-column:2;
+  }
+`;
+
+export const Banner = styled.div`
+display:flex;
+flex-direction:column;
+gap:1rem;
+
+h1{
+  color:${({theme})=> theme['base-title']};
+  font-size:3rem;
+  font-family: "Baloo 2", sans-serif;
+  line-height:130%;
+  font-weight: 800;
+}
+
+p{
+  color:${({theme})=> theme['base-subtitle']};
+  font-size:1.25rem;
+  font-weight: 400;
+  line-height:130%;
+}
+
+`;
+
+export const InfoContainer = styled.div`
+max-width:560px;
+display:flex;
+align-items:center;
+gap:1rem;
+flex-wrap:wrap;
+margin-top:-4.5rem;
+
+img{
+  margin-top:-2rem;
+}
+
+span{
+  color:${({theme})=> theme['base-text']};
+  font-size:1rem;
+  font-weight: 400;
+  line-height: 130%;
+  margin-bottom:2rem;
+  
+}
+`;
