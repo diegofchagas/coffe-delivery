1a22e5ecd60eb28284f39b48773c2353729d84fd
Diego Chagas <diegofchagas@gmail.com>
2024-02-20 14:58:04 -0300

    excluindo rota desnecessaria

--- src/Router.tsx
+++ src/Router.tsx
@@ -3,7 +3,6 @@ import {Routes, Route} from "react-router-dom"
 import { DefaultLayout } from "./Layout/DefaultLayout"
 
 import { Checkout } from "./pages/Checkout"
-import { CheckoutFiled } from "./pages/CheckoutFiled"
 import { Success } from "./pages/Success"
 import { Home } from "./pages/Home"
 
@@ -15,7 +14,6 @@ export const Router = () => {
       <Route path="/" element={<DefaultLayout/>}>
       <Route path="/" element={<Home/>}/>
         <Route path="/checkout" element={<Checkout/>}/>
-        <Route path="/checkoutFiled" element={<CheckoutFiled/>}/>
         <Route path="/success" element={<Success/>}/>
       </Route>
     </Routes>
