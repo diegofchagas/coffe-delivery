95ddd0f160d0a00d7ec8c3d67fbb32bca35e5930
Diego Chagas <diegofchagas@gmail.com>
2024-02-20 14:37:33 -0300

    add nova pagina home e estilizacao da mesma juntamente com o componente card coffe onde é mostrado todos os cafes disponiveis para comprar

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,10 +1,21 @@
-import React from 'react'
-import { About } from '../../components/About'
+import { About } from "../../components/About";
+import { CardCoffe } from "./components/CardCoffe";
+import {coffeTypes} from "../../DataCoffeTypes/coffesTypes"
+
+import { ContainerHome } from "./styles";
+
 
 export const Home = () => {
   return (
-    <div>
-        <About/>
-    </div>
-  )
-}
+    <ContainerHome>
+      <About />
+      <h1>Nossos cafés</h1>
+
+      <section className="teste">
+      {coffeTypes.map(coffes => <CardCoffe coffes={coffes} /> )}
+      <img src="../../assets/coffes/arabe.svg" alt="" />
+      </section>
+     
+    </ContainerHome>
+  );
+};
---
+++ src/pages/Home/styles.ts
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const ContainerHome = styled.main`
+margin-left:2rem;
+
+.teste{
+  display:grid;
+grid-template-columns: 1fr 1fr 1fr 1fr;
+gap:1rem;
+}
+
+`;
