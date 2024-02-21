2f3ba4706bbd6a50d1368e3c62407216b60a5afe
Diego Chagas <diegofchagas@gmail.com>
2024-02-21 20:48:47 -0300

    mudando componente card coffe de pasta

---
+++ src/components/CardCoffe/index.tsx
@@ -0,0 +1,37 @@
+import { ShoppingCartSimple } from "@phosphor-icons/react";
+import {CardContainer, CarrinhoButton, FooterContainer } from "./style";
+import { QuantityButton } from "../QuantityButton";
+
+interface CoffeTypes {
+  image: string;
+  type: string[];
+  name: string;
+  description: string;
+}
+
+interface CardCoffeProps {
+  coffes: CoffeTypes;
+}
+
+export const CardCoffe = ({ coffes }: CardCoffeProps) => {
+
+  return (
+        <CardContainer>
+          <img  src={coffes.image} alt={coffes.name} />
+          <div style={{display: 'flex', gap: '.625rem'}}>
+          {coffes.type.map(type =><span className="types">{type}</span>)}
+          </div>
+          <strong>{coffes.name}</strong>
+          <p>
+          {coffes.description} 
+          </p>
+          <FooterContainer>
+          <small className="price">R$ <strong>9,90</strong></small>
+          <QuantityButton/>
+          <CarrinhoButton>
+          <ShoppingCartSimple size={22} weight="fill" color="white"  />
+          </CarrinhoButton>
+          </FooterContainer>
+        </CardContainer>
+  );
+};
---
+++ src/components/CardCoffe/style.ts
@@ -0,0 +1,75 @@
+import styled from "styled-components";
+
+export const CardContainer = styled.div`
+  max-width: 16rem;
+  padding: 10px 20px;
+  background-color: ${({ theme }) => theme["base-card"]};
+  border-radius: 6px 36px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 1rem;
+  margin-top: 2rem;
+
+  img {
+    margin-top: -2rem;
+    width: 7.5rem;
+    height: 7.5rem;
+  }
+
+  .types {
+    background-color: ${({ theme }) => theme["yellow-light"]};
+    padding: 4px 8px;
+    border-radius: 999px;
+    font-size: 0.625rem;
+    font-weight: 700;
+    line-height: 130%;
+    color: ${({ theme }) => theme["yellow-dark"]};
+    text-transform: uppercase;
+  }
+
+  strong {
+    color: ${({ theme }) => theme["base-subtitle"]};
+    font-family: "Baloo 2", sans-serif;
+    font-size: 1.25rem;
+    line-height: 130%;
+    font-weight: 700;
+  }
+
+  p {
+    color: ${({ theme }) => theme["base-label"]};
+    font-size: 0.875rem;
+    line-height: 130%;
+    text-align: center;
+  }
+`;
+
+export const FooterContainer = styled.div`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  .price {
+    font-size: 0.875rem;
+    color: ${({ theme }) => theme["base-text"]};
+    font-weight: 400;
+    line-height: 130%;
+  }
+
+  strong {
+    font-family: "Baloo 2", sans-serif;
+    font-size: 1.5rem;
+    font-weight: 800;
+    line-height: 130%;
+    padding-right: 18px;
+  }
+`;
+
+export const CarrinhoButton = styled.button`
+  border: none;
+  background-color: ${({ theme }) => theme["purle-dark"]};
+  padding: 8px;
+  border-radius: 6px;
+  cursor: pointer;
+`;
