ee747f7e382005abf90fc95ce5da6a3b15bf7273
Diego Chagas <diegofchagas@gmail.com>
2024-02-21 20:48:21 -0300

    adicionando estrutura e estilizacoes ao component chekout

--- src/pages/Checkout/index.tsx
+++ src/pages/Checkout/index.tsx
@@ -1,7 +1,56 @@
-import React from 'react'
+import { AdressContainerComponent } from "./components/AdressContainer";
+import { ButtonComponent } from "./components/ButtonCard";
+import { CardCoffeSelection } from "./components/CardCoffeSelection";
+import {
+  ChekoutContainer,
+  CoffeSelected,
+  SelectContainer,
+  Footer,
+  ConfirmOrder,
+  PaymentsContainer,
+} from "./style";
+
+import {
+  Bank,
+  CreditCard,
+  CurrencyDollar,
+  Money,
+} from "@phosphor-icons/react";
 
 export const Checkout = () => {
   return (
-    <div>Checkout</div>
-  )
-}
+    <ChekoutContainer>
+      <AdressContainerComponent/>
+      <CoffeSelected>
+        <h3>Cafés selecionados</h3>
+        <SelectContainer>
+          <CardCoffeSelection />
+          <Footer>
+            <span>Total de itens</span>
+            <span className="value">R$ 29,70</span>
+            <span>Entrega</span>
+            <span className="value">R$ 3,50</span>
+            <strong>Total</strong>
+            <strong className="value">R$33,20</strong>
+            <ConfirmOrder>Confirmar Pedido</ConfirmOrder>
+          </Footer>
+        </SelectContainer>
+      </CoffeSelected>
+
+      <PaymentsContainer>
+        <p>
+          <CurrencyDollar size={22} color="#8047F8" />
+          Pagamento
+        </p>
+        <span>
+          O pagamento é feito na entrega. Escolha a forma que deseja pagar
+        </span>
+        <div>
+          <ButtonComponent title=" Cartão de crédito" Icon={CreditCard} size={16}/>
+          <ButtonComponent title=" Cartão de débito" Icon={Bank} size={16}/>
+          <ButtonComponent title=" dinheiro" Icon={Money} size={16}/>
+        </div>
+      </PaymentsContainer>
+    </ChekoutContainer>
+  );
+};
