439a5a83920392717ba36793a32dad53737fb870
Diego Chagas <diegofchagas@gmail.com>
2024-03-05 10:24:53 -0300

    add react hook form ao componente address para validacao dos inputs

--- src/pages/Checkout/components/AdressContainer/index.tsx
+++ src/pages/Checkout/components/AdressContainer/index.tsx
@@ -1,33 +1,102 @@
-import {MapPinLine} from "@phosphor-icons/react";
+import { MapPinLine } from "@phosphor-icons/react";
+import {
+  /*UseFormRegister, FieldValues,*/ useFormContext,
+} from "react-hook-form";
 
-import {AdressContainer,FormContainer,InputsContainer,CepInput,StateInput,StreetInput,NumberInput,ComplementInput, DistrictInput, CityInput} from './style'
+import {
+  AdressContainer,
+  FormContainer,
+  InputsContainer,
+  CepInput,
+  StateInput,
+  StreetInput,
+  NumberInput,
+  ComplementInput,
+  DistrictInput,
+  CityInput,
+} from "./style";
 
-export const AdressContainerComponent = () => {
-  return (
-    <AdressContainer>
-    <h3>Complete seu pedido</h3>
-    <FormContainer>
-      <header>
-        <p>
-          <MapPinLine size={22} color="#C47F17" />
-          Endereço de Entrega
-        </p>
-        <span>Informe o endereço onde deseja receber seu pedido</span>
-      </header>
-      <InputsContainer>
-        <CepInput type="number" placeholder="CEP" />
-        <StreetInput type="text" placeholder="Rua" />
-        <div>
-          <NumberInput type="number" placeholder="Número" />
-          <ComplementInput type="text" placeholder="Complemento" />
-        </div>
-        <div>
-          <DistrictInput type="text" placeholder="Bairro" />
-          <CityInput type="text" placeholder="Cidade" />
-          <StateInput type="text" placeholder="UF" />
-        </div>
-      </InputsContainer>
-    </FormContainer>
-  </AdressContainer>
-  )
+// interface AdressContainerComponentProps {
+//   register: UseFormRegister<FieldValues>;
+// }
+
+interface FormErrors {
+  address?: {
+    zipCode?: string;
+    street?: string;
+    number?: string;
+    complement?: string;
+    district?: string;
+    city?: string;
+    stateOf?: string;
+  };
 }
+
+export const AdressContainerComponent =
+  (/*{register}: AdressContainerComponentProps*/) => {
+    const {
+      register,
+      formState: { errors },
+    } = useFormContext<FormErrors>();
+
+    return (
+      <AdressContainer>
+        <h3>Complete seu pedido</h3>
+        <FormContainer>
+          <header>
+            <p>
+              <MapPinLine size={22} color="#C47F17" />
+              Endereço de Entrega
+            </p>
+            <span>Informe o endereço onde deseja receber seu pedido</span>
+          </header>
+          <InputsContainer>
+            <CepInput
+              type="text"
+              placeholder="CEP"
+              {...register("address.zipCode")}
+              maxLength={9}
+            />
+            {errors.address?.zipCode?.message && (
+              <span className="error"> {errors.address.zipCode.message}</span>
+            )}
+
+            <StreetInput
+              type="text"
+              placeholder="Rua"
+              {...register("address.street")}
+            />
+            <div>
+              <NumberInput
+                type="text"
+                placeholder="Número"
+                {...register("address.number")}
+              />
+              <ComplementInput
+                type="text"
+                placeholder="Complemento"
+                {...register("address.complement")}
+              />
+            </div>
+            <div>
+              <DistrictInput
+                type="text"
+                placeholder="Bairro"
+                {...register("address.district")}
+              />
+              <CityInput
+                type="text"
+                placeholder="Cidade"
+                {...register("address.city")}
+              />
+              <StateInput
+                type="text"
+                placeholder="UF"
+                {...register("address.stateOf")}
+              />
+            </div>
+          </InputsContainer>
+        </FormContainer>
+      </AdressContainer>
+    );
+  };
