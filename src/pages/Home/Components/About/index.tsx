a4963063fb4381a1d16361ade78428f41357411f
Diego Chagas <diegofchagas@gmail.com>
2024-03-07 11:24:24 -0300

    add background a home e alterando estrutua do codigo

--- src/pages/Home/Components/About/index.tsx
+++ src/pages/Home/Components/About/index.tsx
@@ -1,30 +1,47 @@
 import copo from "../../../../assets/copo-cafe.svg";
-import carrinho from "../../../../assets/carrinho.svg";
-import embalagem from "../../../../assets/embalagem.svg";
-import relogio from "../../../../assets/relogio.svg";
-import cafe from "../../../../assets/cafe.svg";
-import { Banner, ContainerAbout, InfoContainer } from "./style";
+import { Banner, Container, ContainerAbout, InfoContainer } from "./style";
+import { TextWithIcon } from "../../../../components/TextWithIcon";
+import { Coffee, Package, ShoppingCart, Timer } from "@phosphor-icons/react";
+
 
 export const About = () => {
   return (
-    <ContainerAbout>
-      <Banner>
-        <h1>
-          Encontre o café perfeito <br />
-          para qualquer hora do dia
-        </h1>
-        <p>
-          Com o Coffee Delivery você recebe seu café onde estiver, a <br />{" "}
-          qualquer hora
-        </p>
-      </Banner>
-      <img src={copo} alt="" />
-      <InfoContainer>
-        <img src={carrinho} alt="" /> <span>Compra simples e segura</span>
-        <img src={embalagem} alt="" /> <span>Embalagem mantém o café intacto</span>
-        <img src={relogio} alt="" /> <span>Entrega rápida e rastreada</span>
-        <img src={cafe} alt="" /> <span>O café chega fresquinho até você</span>
-      </InfoContainer>
-    </ContainerAbout>
+    <Container>
+      <ContainerAbout>
+        <Banner>
+          <h1>
+            Encontre o café perfeito <br />
+            para qualquer hora do dia
+          </h1>
+          <p>
+            Com o Coffee Delivery você recebe seu café onde estiver, a <br />{" "}
+            qualquer hora
+          </p>
+        </Banner>
+        <img src={copo} alt="" />
+        <InfoContainer>
+          <TextWithIcon
+            iconBg="#C47F17"
+            icon={<ShoppingCart weight="fill" />}
+            text="Compra simples e segura"
+          />
+          <TextWithIcon
+            iconBg="#574F4D"
+            icon={<Package weight="fill" />}
+            text="Embalagem mantém o café intacto"
+          />
+          <TextWithIcon
+            iconBg="#DBAC2C"
+            icon={<Timer weight="fill" />}
+            text="Entrega rápida e rastreada"
+          />
+          <TextWithIcon
+            iconBg="#8047F8"
+            icon={<Coffee weight="fill" />}
+            text="O café chega fresquinho até você"
+          />
+        </InfoContainer>
+      </ContainerAbout>
+    </Container>
   );
 };
--- src/pages/Home/Components/About/style.ts
+++ src/pages/Home/Components/About/style.ts
@@ -1,12 +1,25 @@
 import styled from 'styled-components';
+import aboutBackground from '../../../../assets/about-background.svg'
+
+
+export const Container = styled.div`
+  width:100%;
+  background: ${() => `url(${aboutBackground}) no-repeat center,
+  linear-gradient(0deg, #fff 0%, rgba(250, 250, 250, 0.2) 50%, #fafafa 100%)
+  `};
+  background-size:cover;
+  margin-bottom:9rem;
+`;
 
 export const ContainerAbout = styled.section`
+  width:100%;
+  max-width:1120px;
+  margin-right: auto;
+  margin-left: auto;
   margin-top:5rem;
-  margin-bottom:9rem;
   display:grid;
   grid-template-columns: 2fr 1fr;
- 
-  
+
   img{
     grid-column:2;
   }
@@ -31,29 +44,12 @@ p{
   font-weight: 400;
   line-height:130%;
 }
-
 `;
 
 export const InfoContainer = styled.div`
-max-width:560px;
-display:flex;
-align-items:center;
-gap:1rem;
-flex-wrap:wrap;
-margin-top:-4rem;
-
-
-
-img{
-  margin-top:-2rem;
-}
-
-span{
-  color:${({theme})=> theme['base-text']};
-  font-size:1rem;
-  font-weight: 400;
-  line-height: 130%;
-  margin-bottom:2rem;
-  
-}
+width:100%;
+display:grid;
+grid-template-columns: 1fr 1fr;
+row-gap:1.25rem;
+margin-top:-4.125rem;
 `;
