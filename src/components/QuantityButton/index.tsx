edfee8e7e4aad6f33e0b9d0df51e35bf25835884
Diego Chagas <diegofchagas@gmail.com>
2024-03-11 16:25:54 -0300

    add props ao componente quantityButton

--- src/components/QuantityButton/index.tsx
+++ src/components/QuantityButton/index.tsx
@@ -1,14 +1,21 @@
 import { Plus, Minus } from "@phosphor-icons/react";
 import { AmountContainer, Button } from "./style";
 
-export const QuantityButton = () => {
+interface QuantityButtonProps{
+  onIncrement: () => void;
+  onDecrement: () => void;
+  quantity:number
+}
+
+export const QuantityButton = ({onIncrement,onDecrement,quantity}:QuantityButtonProps) => {
+
   return (
     <AmountContainer>
-      <Button>
+      <Button disabled={quantity <= 1} onClick={onDecrement}>
         <Minus size={14} weight="bold" />
       </Button>
-      <span className="quantity">2</span>
-      <Button>
+      <span className="quantity">{quantity}</span>
+      <Button onClick={onIncrement}>
         <Plus size={14} weight="bold" />
       </Button>
     </AmountContainer>
