eaf042a0ee4a7db8d253a86c5bde4862e83c83ff
Diego Chagas <diegofchagas@gmail.com>
2024-03-11 16:25:27 -0300

    add context ao cardCoffee

--- src/pages/Home/Components/CardCoffe/index.tsx
+++ src/pages/Home/Components/CardCoffe/index.tsx
@@ -1,16 +1,17 @@
 import { ShoppingCartSimple } from "@phosphor-icons/react";
-import {CardContainer, CardFooter, CarrinhoButton } from "./style";
+import { CardContainer, CardFooter, CarrinhoButton } from "./style";
 import { QuantityButton } from "../../../../components/QuantityButton";
 import { formatMoney } from "../../../../utils/formatMoney";
-
+import { useContext, useState } from "react";
+import { CartContext } from "../../../../contexts/CartContext";
 
 export interface CoffeTypes {
-  id:number,
+  id: number;
   image: string;
   type: string[];
   name: string;
   description: string;
-  price:number
+  price: number;
 }
 
 interface CardCoffeProps {
@@ -18,25 +19,51 @@ interface CardCoffeProps {
 }
 
 export const CardCoffe = ({ coffes }: CardCoffeProps) => {
- const formattedPrice = formatMoney(coffes.price)
+  const [ quantity, setQuantity] = useState(1);
+
+  const { addCoffeeToCart } = useContext(CartContext);
+
+  function handleIncrement() {
+    setQuantity((prevState) => prevState + 1);
+  }
+
+  function handleDecrement() {
+    setQuantity((prevState) => prevState - 1);
+  }
+
+  function handleToCart() {
+    const coffeeToAdd = {
+      ...coffes,
+      quantity
+    };
+    addCoffeeToCart(coffeeToAdd);
+  }
+
+  const formattedPrice = formatMoney(coffes.price);
 
   return (
-        <CardContainer>
-          <img  src={coffes.image} alt={coffes.name} />
-          <div style={{display: 'flex', gap: '.625rem'}}>
-          {coffes.type.map(type =><span className="types">{type}</span>)}
-          </div>
-          <strong>{coffes.name}</strong>
-          <p>
-          {coffes.description} 
-          </p>
-          <CardFooter>
-          <small className="price">R$ <strong>{formattedPrice}</strong></small>
-          <QuantityButton/>
-          <CarrinhoButton>
-          <ShoppingCartSimple size={22} weight="fill" color="white"  />
-          </CarrinhoButton>
-          </CardFooter>
-        </CardContainer>
+    <CardContainer>
+      <img src={coffes.image} alt={coffes.name} />
+      <div style={{ display: "flex", gap: ".625rem" }}>
+        {coffes.type.map((type) => (
+          <span className="types">{type}</span>
+        ))}
+      </div>
+      <strong>{coffes.name}</strong>
+      <p>{coffes.description}</p>
+      <CardFooter>
+        <small className="price">
+          R$ <strong>{formattedPrice}</strong>
+        </small>
+        <QuantityButton
+        onIncrement={handleIncrement}
+        onDecrement ={handleDecrement}
+        quantity={quantity}
+        />
+        <CarrinhoButton onClick={handleToCart}>
+          <ShoppingCartSimple size={22} weight="fill" color="white" />
+        </CarrinhoButton>
+      </CardFooter>
+    </CardContainer>
   );
 };
