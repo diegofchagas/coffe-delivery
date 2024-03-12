3fa8c44ca9b64be8cf016d5f904b83c17f9b0c09
Diego Chagas <diegofchagas@gmail.com>
2024-03-12 12:02:30 -0300

    adicionando novas funcionalidades ao context contangem de cafes no carrinhos valor total dos mesmos

--- src/contexts/CartContext.tsx
+++ src/contexts/CartContext.tsx
@@ -7,9 +7,13 @@ export interface CartItem extends CoffeTypes{
 
 interface CartContextType{
   cartItems:CartItem[];
+  cartCoffesQuantity:number,
+  totalValueCoffes:number,
   addCoffeeToCart: (coffee:CartItem) => void;
-  cartCoffesQuantity:number
+  changeCartItemQuantity: (cartItemId:number, type:"increment" | "decrement")=> void;
+  removeCartItem: (cartItemId:number)=> void;
 }
+
 
 interface CartContextProviderProps{
   children:ReactNode;
@@ -21,6 +25,10 @@ export const CartContextProvider = ({ children }: CartContextProviderProps) => {
   const [cartItems, setCartItems] = useState<CartItem[]>([]);
 
   const cartCoffesQuantity = cartItems.length;
+  
+
+  const totalValueCoffes = cartItems.reduce((total,cartItem)=> { return total + cartItem.price * cartItem.quantity},0)
+  
 
   function addCoffeeToCart(coffee: CartItem) {
     const itemExistsCart = cartItems.findIndex((item) => item.id === coffee.id);
@@ -37,9 +45,46 @@ export const CartContextProvider = ({ children }: CartContextProviderProps) => {
       );
     }
   }
+
+  function changeCartItemQuantity(cartItemId:number, type: "increment" | "decrement"){
+    // const newCart = cartItems.map(cartItem => {
+    //   if (cartItem.id === cartItemId) {
+    //     const quantity = type === "increment" ? cartItem.quantity + 1 : cartItem.quantity - 1;
+    //     return { ...cartItem, quantity };
+    //   }
+    //   return cartItem;
+    // });
+  
+    // setCartItems(newCart);
+    const coffeExistsInCart = cartItems.findIndex(cartItem => cartItem.id === cartItemId)
+
+    if(coffeExistsInCart >= 0){
+      const newCartItems = cartItems.map((item,index)=> {
+        if(index !== coffeExistsInCart){
+          return item;
+        }
+
+        return{
+          ...item,
+          quantity:type === 'increment' ? item.quantity + 1 : item.quantity - 1
+        }
+        
+      })
+      setCartItems(newCartItems)
+    }
+  }
+
+  function removeCartItem(cartItemId:number){
+    const coffeExistsInCart = cartItems.findIndex(cartItem => cartItem.id === cartItemId)
+
+    if(coffeExistsInCart >= 0){
+      const newCartItems = cartItems.filter(cartItem => cartItem.id !== cartItemId)
+      setCartItems(newCartItems)
+    }  
+  }
   console.log(cartItems)
   return (
-    <CartContext.Provider value={{ cartItems, addCoffeeToCart,cartCoffesQuantity}}>
+    <CartContext.Provider value={{ cartItems, addCoffeeToCart,cartCoffesQuantity,changeCartItemQuantity,removeCartItem,totalValueCoffes}}>
       {children}
     </CartContext.Provider>
   );
